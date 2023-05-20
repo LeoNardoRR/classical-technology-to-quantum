8a6a15a1ff6a39e122a217c2d9447353035acdda
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-05-20 19:08:25 -0300

    update py

--- DbOperations/main.py
+++ DbOperations/main.py
@@ -1,9 +1,39 @@
-import qsharp
-from DbOperations import SayHello
+#import qsharp
+import mysql.connector
+#from DbOperations import SayHello
 
-print(SayHello.simulate(name="Leonardo Ribeiro"))
+#print(SayHello.simulate(name="Leonardo Ribeiro"))
 
 # 1. Trazer os usuários (SELECT)
+# Estabelece a conexão com o banco de dados MySQL
+con = mysql.connector.connect(
+    host='localhost',
+    database='aes',
+    user='root',
+    password='123456'
+)
+
+if con.is_connected():
+    cursor = con.cursor()
+    
+    # Executa a consulta SELECT
+    cursor.execute("SELECT * FROM ic WHERE ano=1994")
+    
+    # Obtém todos os registros da consulta
+    rows = cursor.fetchall()
+    
+    # Itera sobre os registros e exibe os resultados
+    for row in rows:
+        print(row)
+    
+    cursor.close()
+    con.close()
+
+# if con.is_connected():
+#    cursor.close()
+#    con.close()
+#    print("Conexão ao MySQL foi encerrada")
+
 # 2. Pegar para cada um desses um usuários e tentar realizar a descriptografia da sua senha
 # usando o código quantico que vai ser criado no Q# (DbOperations.BreakPassword('123456'))
 # 3. Verificar se foi bem sucedido e também quanto tempo levou
