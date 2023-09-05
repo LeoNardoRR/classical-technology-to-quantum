c0d172d5827d1f8755b8d2b54412f07f7463588e
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-09-05 18:39:38 -0300

    fatoração quadratica

--- FATECSEG/rsa/crivo.py
+++ FATECSEG/rsa/crivo.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def calcular_mdc(a, b):
     while b:
@@ -29,5 +30,13 @@ def fatoracao_quadratica(numero):
 
 # Solicite ao usuário que insira um número para realizar a fatoração em primos
 numero_para_fatorar = int(input("Digite um número para fatorar em primos: "))
+
+# Medir o tempo de execução
+inicio = time.time()
 fatores_primos = fatoracao_quadratica(numero_para_fatorar)
+fim = time.time()
+
+tempo_decorrido = fim - inicio
+
 print(f"Os fatores primos de {numero_para_fatorar} são: {fatores_primos}")
+print(f"Tempo decorrido: {tempo_decorrido} segundos")
