22495d1bdf173a11ebb04b2ec6e1565ebcf69dce
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-09-05 18:37:00 -0300

    update all

---
+++ FATECSEG/rsa/crivo.py
@@ -0,0 +1,33 @@
+import math
+
+def calcular_mdc(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def fatoracao_quadratica(numero):
+    if numero < 2:
+        return [numero]
+
+    fatores_primos = []
+    while numero % 2 == 0:
+        fatores_primos.append(2)
+        numero //= 2
+
+    if numero == 1:
+        return fatores_primos
+
+    for a in range(3, int(math.sqrt(numero)) + 1, 2):
+        while numero % a == 0:
+            fatores_primos.append(a)
+            numero //= a
+
+    if numero > 1:
+        fatores_primos.append(numero)
+
+    return fatores_primos
+
+# Solicite ao usuário que insira um número para realizar a fatoração em primos
+numero_para_fatorar = int(input("Digite um número para fatorar em primos: "))
+fatores_primos = fatoracao_quadratica(numero_para_fatorar)
+print(f"Os fatores primos de {numero_para_fatorar} são: {fatores_primos}")
