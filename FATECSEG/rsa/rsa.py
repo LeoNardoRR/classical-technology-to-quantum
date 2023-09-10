80537e6b6cbb734ab5a3fb2f1c9fbb430f0c9f0b
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-09-10 18:30:44 -0300

    update rsa

--- FATECSEG/rsa/rsa.py
+++ FATECSEG/rsa/rsa.py
@@ -3,24 +3,18 @@ from cryptography.hazmat.primitives import serialization, hashes
 import getpass
 import time
 
-# Registrar o tempo de início
 inicio = time.time()
 
-# Carregar chave pública do destinatário a partir de um arquivo PEM
 with open('public_key.pem', 'rb') as f:
     public_key = serialization.load_pem_public_key(f.read())
 
-# Carregar chave privada do remetente a partir de um arquivo PEM
 with open('private_key.pem', 'rb') as f:
     private_key = serialization.load_pem_private_key(f.read(), password=None)
 
-# Solicitar que o usuário digite o texto a ser criptografado
 mensagem = getpass.getpass("Digite a senha/texto a ser criptografado: ")
 
-# Converter a mensagem em bytes usando a codificação UTF-8
 mensagem_bytes = mensagem.encode("utf-8")
 
-# Criptografar a mensagem com a chave pública
 ciphertext = public_key.encrypt(
     mensagem_bytes,
     padding.OAEP(
@@ -30,7 +24,6 @@ ciphertext = public_key.encrypt(
     )
 )
 
-# Descriptografar o texto cifrado com a chave privada
 plaintext_bytes = private_key.decrypt(
     ciphertext,
     padding.OAEP(
@@ -45,8 +38,6 @@ plaintext = plaintext_bytes.decode("utf-8")
 print("Texto cifrado:", ciphertext)
 print("Texto decifrado:", plaintext)
 
-# Registrar o tempo de término
 fim = time.time()
 
-# Calcular o tempo de execução
 tempo_execucao = fim - inicio
