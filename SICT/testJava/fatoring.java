8aa99d106f2e1f74918ef232602c6f37a87bca71
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-10-12 16:07:35 -0300

    update fatoring

--- SICT/testJava/fatoring.java
+++ SICT/testJava/fatoring.java
@@ -1,41 +1,41 @@
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
-public class Descriptografar {
+public class fatoring {
 
     public static void main(String[] args) {
-        // Gerar uma chave aleatória a partir da semente
         SecureRandom secureRandom = new SecureRandom();
         byte[] chaveBytes = new byte[16]; // 16 bytes é um tamanho comum para chaves
         secureRandom.nextBytes(chaveBytes);
 
-        // Converter a chave em formato Base64
         String chaveBase64 = new String(java.util.Base64.getEncoder().encode(chaveBytes));
 
-        // Exibir a chave gerada
         System.out.println("Chave gerada: " + chaveBase64);
 
-        // Usar a chave como semente para gerar números primos aleatórios
         secureRandom = new SecureRandom(chaveBytes);
 
-        // Gerar dois números primos aleatórios
-        BigInteger p = generateRandomPrime(secureRandom, 32); // 128 bits é um tamanho comum
+        BigInteger p = generateRandomPrime(secureRandom, 32);
         BigInteger q = generateRandomPrime(secureRandom, 32);
 
-        // Calcular o número composto (produto dos números primos)
         BigInteger numeroComposto = p.multiply(q);
 
-        // Exibir os números primos e o número composto
         System.out.println("Numero Primo p: " + p);
         System.out.println("Numero Primo q: " + q);
         System.out.println("Numero Composto: " + numeroComposto);
 
-        // Fatorar o número composto usando o Algoritmo de Fermat
+        long startTime = System.currentTimeMillis(); // Registra o tempo inicial
+
         System.out.print("Fatores primos do numero composto: ");
         BigInteger[] fatores = fatorarFermat(numeroComposto);
         for (BigInteger fator : fatores) {
             System.out.print(fator + " ");
         }
+
+        long endTime = System.currentTimeMillis(); // Registra o tempo final
+
+        long duration = endTime - startTime; // Calcula a duração da fatoração em milissegundos
+
+        System.out.println("\nTempo de fatoracao (milissegundos): " + duration);
     }
 
     public static BigInteger generateRandomPrime(SecureRandom secureRandom, int bitLength) {
